8605ba7a97e2e035f09f271315946ce484789b05
Adil <adil.tabrez@gmail.com>
2024-12-31 02:42:33 -0500

    First draft - Lecture 1

---
+++ Lecture1.py
@@ -0,0 +1,17 @@
+# # Write a program to input 2 numbers and print their sum 
+
+# inum1 = int(input("Enter First Number: "))
+# inum2 = int(input("Enter Second Number: "))
+
+# rnum = inum1 + inum2 
+
+# print(rnum) 
+
+# # Write a program to input side of a square and print its area 
+
+# iside = int(input("Enter Square's Side: "))
+
+# rarea = iside*iside 
+
+# print(rarea)
+
